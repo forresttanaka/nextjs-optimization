d38c12b600188d3cf10724f4798a0bf77ec0dd92
forresttanaka <fytanaka@stanford.edu>
2022-06-21 12:35:14 -0700

    Add components to test useState optimization

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const editable = (session, item) => {
+  console.log("Session %s", session);
+  console.log("Item %s", item);
+  return { canDo: Boolean(session && item) };
+};
+
+const TestComponent = ({ session, item }) => {
+  const [editorStatus, setEditorStatus] = useState({
+    canEdit: editable(session, item),
+    canSave: editable(session, item),
+  });
+
+  return (
+    <>
+      <div>Session: {session}</div>
+      <div>Item: {item}</div>
+      <div>State: {JSON.stringify(editorStatus)}</div>
+    </>
+  );
+};
+
+const Controller = () => {
+  const [session, setSession] = useState(0);
+  const [item, setItem] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setSession(session + 1)}>Session</button>
+      <button onClick={() => setItem(item + 2)}>Test</button>
+      <TestComponent session={session} item={item} />
+    </>
+  );
+};
 
 export default function Home() {
   return (
@@ -12,58 +47,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Controller />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
